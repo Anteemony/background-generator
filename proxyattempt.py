89465b019850771f5d8310a6032e53736bacefd2
Anthony <Anteemony@gmail.com>
2022-08-16 00:26:03 +0100

    added big feature documentation comment

--- proxyattempt.py
+++ proxyattempt.py
@@ -11,6 +11,9 @@ from flask import Flask, render_template, request, abort, Response, redirect
 import requests
 import logging
 
+
+"This is a pretty big feature"
+
 app = Flask(__name__.split('.')[0])
 logging.basicConfig(level=logging.INFO)
 APPROVED_HOSTS = set(["ww1.google.com", "ww1.google.com", "yahoo.com", "wcofun.com", "torrentgalaxy.to"])
